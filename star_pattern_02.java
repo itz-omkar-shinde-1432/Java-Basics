a0b0735a25d8412c38d68e1abe3dc60f1db7f60c
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-02 22:53:21 +0530

    Added star_pattern_02.java

---
+++ star_pattern_02.java
@@ -0,0 +1,9 @@
+public class star_pattern_02 {
+    public static void main (String args []){
+      System.out.println("*");
+      System.out.println("**");
+      System.out.println("***");
+      System.out.println("****");
+    }
+    
+}
