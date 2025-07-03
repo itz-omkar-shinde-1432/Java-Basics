985ec29655fff9c57ccc131d9f7ccb411e410cfd
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-03 16:01:59 +0530

    Added UserDetails.java

---
+++ UserDetails.java
@@ -0,0 +1,19 @@
+import java.util.Scanner ;
+public class UserDetails {
+    public static void main(String args[]){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Enter Your Name:");
+        String name= input.nextLine();
+
+        System.out.println("Enter Your age:");
+        int age= input.nextInt();
+         
+        System.out.println("Hello "+name+"! You are "+age+" Years Old.");
+
+        input.close();
+
+
+    }
+    
+}
