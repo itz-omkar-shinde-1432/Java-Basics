2da5a00ffeb3c02694e0b9b985daa77248ffdab1
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-17 20:10:18 +0530

    Added new pattern

--- patterns/InvertedStarTriangle.java
+++ patterns/InvertedStarTriangle.java
@@ -8,7 +8,7 @@ public class InvertedStarTriangle {
         int n= sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i+1;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
