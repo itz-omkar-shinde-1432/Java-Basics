493c209509949a48e52053e424dcd70814795096
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-17 19:37:33 +0530

    Added new pattern

---
+++ patterns/InvertedStarTriangle.java
@@ -0,0 +1,17 @@
+package patterns;
+import java.util.Scanner;
+
+public class InvertedStarTriangle {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number of rows for the inverted star triangle: ");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
--- patterns/RepeatedNumberTriangle.java
+++ patterns/RepeatedNumberTriangle.java
@@ -9,7 +9,6 @@ public class RepeatedNumberTriangle {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(i+" ");
-
             }
             System.out.println();
         }
