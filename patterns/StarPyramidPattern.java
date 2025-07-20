f87f42767f14c5c31959f2f6189f2526dd6b29b8
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-20 19:38:07 +0530

    Added StarPyramidPattern.java

--- patterns/StarPyramidPattern.java
+++ patterns/StarPyramidPattern.java
@@ -1,6 +1,26 @@
 package patterns;
-
+import java.util.Scanner;
 public class StarPyramidPattern {
-    
+    Scanner sc = new Scanner (System.in);
+    public static void main(String[] args) {
+        StarPyramidPattern pattern = new StarPyramidPattern();
+        System.out.print("Enter the number of rows for the star pyramid: ");
+        int n = pattern.sc.nextInt();
+        pattern.printStarPyramid(n);
+    }
+    public void printStarPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
     
 }
