d797f89bf84ce73cf1ef829bd452a0aade84919a
Omkar Shinde <omkar.shinde.work.dev@gmail.com>
2025-07-22 19:09:00 +0530

    Added new Pattern

---
+++ patterns/DiamondPyramid.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class DiamondPyramid {
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the diamond pyramid: ");
+        int n=input.nextInt();
+
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }   
+            System.out.println();
+        }
+
+        for(int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*(n-i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
